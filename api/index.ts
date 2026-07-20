29a273665f58b38794cf7197da7be95d5c179d5a
mgomez <matisse.gomez@hubone.fr>
2026-07-20 12:57:09 +0200

    update

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import app from "../src/app";
-import { connectDB } from "../src/db";
-
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
-  await connectDB();
-  app(req, res);
-}
